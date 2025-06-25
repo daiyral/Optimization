8e4ab6c9f44297c20fd430c51a83f15361bfd7e0
Daniel Aibinder <dannygameplay@gmail.com>
2025-06-25 15:52:25 +0300

    large prime

--- msolve/src/msolve/iofiles.c
+++ msolve/src/msolve/iofiles.c
@@ -544,8 +544,7 @@ static void get_characteristic(FILE *fh, char * line, int max_line_size,
       *field_char = (int32_t) tmp_mod;
       /* change 0 to large prime */
       if(tmp_mod == 0){
-        *field_char = 1073741827; /* prime that fits in 32-bit */
-        fprintf(stderr, "Converting characteristic 0 to prime %d for better stability\n", *field_char);
+        *field_char = 2147483647; 
       }
       if(tmp_mod > 2147483647){
         fprintf(stderr, "Warning: characteristic must be 0 or < 2^31\n");
